3c3ec221a84924a60c56d041167bca7f02616b27
Nikolai Prokoschenko <nikolai@prokoschenko.de>
2016-11-18 21:22:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -k $RHNPUSH_SERVER/pub/RHN-ORG-TRUSTED-SSL-CERT > /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT
+curl -k $RHNPUSH_HOST/pub/RHN-ORG-TRUSTED-SSL-CERT > /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT
 
 rhnpush $@
